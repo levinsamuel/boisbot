45dfb31b287fbb96f4d14419fddc468be021640d
Samuel Levin <levinsamuel000@gmail.com>
2018-10-26 15:27:52 +0200

    add setup utils

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="boisbot",
+    version="0.1",
+    packages=find_packages(exclude=['*test.py']),
+    scripts=['scraper.py'],
+    
+    author="Sam",
+    author_email="levinsamuel000@gmail.com",
+    url="https://github.com/levinsamuel/boisbot"
+)
