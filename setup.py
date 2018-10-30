55b95ea24f1b77feb10dea772c0161d5b82e9a17
Samuel Levin <levinsamuel000@gmail.com>
2018-10-30 16:56:23 +0100

    style on setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="boisbot",
-    version="0.1",
-    packages=find_packages(exclude=['test*.py']),
-    scripts=['scraper.py'],
-    
-    author="Sam",
-    author_email="levinsamuel000@gmail.com",
-    url="https://github.com/levinsamuel/boisbot",
+    name='boisbot',
+    version='0.1',
+    packages=find_packages(exclude=['test', 'test*.py']),
+    scripts=['scrape.py', 'fit.py'],
+
+    author='Sam',
+    author_email='levinsamuel000@gmail.com',
+    url='https://github.com/levinsamuel/boisbot',
     install_requires=[
         'keras',
         'tensorflow',
