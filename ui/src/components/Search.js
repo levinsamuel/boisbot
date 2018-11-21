66306aecbf263677d2744cca6e9d17e17ee21956
Samuel Levin <levinsamuel000@gmail.com>
2018-11-20 23:44:33 -0800

    convert to redux form

--- ui/src/components/Search.js
+++ ui/src/components/Search.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col} from 'reactstrap';
+import {Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label,
+  Input, Col} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm} from 'react-redux-form';
 
@@ -7,26 +8,36 @@ class Search extends Component {
 
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
-    return (
 
+    return (
         <div className="container">
           <div className="row">
             <h3>Search for a user</h3>
           </div>
           <div className="row row-content">
-            <form>
+            <LocalForm onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <label for="usersearch">Twitter Username</label>
-                <input className="form-control" type="search" name="username" placeholder="Twitter User"/>
+                <Label htmlFor="usersearch">Twitter Username</Label>
+                <Control.text className="form-control" type="search" name="username"
+                    placeholder="Twitter User" model=".user"/>
 
               </div>
-            </form>
+              <div className="form-group">
+                <Button>Search
+                </Button>
+              </div>
+            </LocalForm>
           </div>
         </div>
     );
+  }
+
+  handleSubmit(values) {
+    console.log('values', values)
   }
 }
 
